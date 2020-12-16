fcd85702689774bd571d23af95cd7c66b95c2b44
paayk <itsabdennacerama@gmail.com>
2020-12-16 18:22:42 +0100

    prntf

--- Exam1/test.c
+++ Exam1/test.c
@@ -12,6 +12,81 @@
 
 #include "get_next_line.h"
 
+int  ft_strlen(char *s)
+{
+    int i;
+
+    i = 0;
+    while (s[i])
+        i++;
+    return (i);
+        
+}
+
+int is_n(char *s)
+{
+    while (*s)
+    {
+        if (*s == '\n')
+            return (1);
+        s++;
+    }
+    return (0);
+}
+
+char *ft_strjoin(char *st1, char *st2)
+{
+    int len1;
+    int len2;
+    int i;
+    char *fill;
+
+    len1 = ft_strlen(st1);
+    len2 = ft_strlen(st2);
+    fill = malloc(sizeof(char) * (len1 + len2 + 1));
+    while (*st1)
+        fill[i++] = *st1++;
+    while (*st1)
+        fill[i++] = *st2++;
+    fill[i] = '\0';
+    return (fill);
+}
+
+char *ft_strdup(char *st)
+{
+    int i;
+    char *buff;
+    int len;
+
+    len = ft_strlen(st) + 1;
+    buff = malloc(sizeof(char) * len);
+    i = 0;
+    while (st[i])
+    {
+        buff[i] = st[i];
+        i++;
+    }
+    buff[i] = '\0';
+    return (buff);
+}
+
+char  *my_strcut(char *str, int end)
+{
+    int i;
+    int len;
+    char *tmp;
+
+    len = ft_strlen(str);
+    if (len > end)
+        len = end;
+    tmp = malloc(sizeof(char) * (len + 1));
+    i = 0;
+    while (*str && len--)
+        tmp[i++] = *str++;
+    tmp[i] = '\0';
+    return (tmp);
+}
+
 int     fill_line(char **content, char **line)
 {
     int end;
@@ -23,7 +98,7 @@ int     fill_line(char **content, char **line)
     if ((*content)[end] == '\n')
     {
         *line = my_strcut(*content, end);
-        tmp = ft_strdup(*content + end + 1);
+        tmp = ft_strdup(*content);
         free(*content);
         *content = tmp;
         return (1);
@@ -36,8 +111,7 @@ int     fill_line(char **content, char **line)
     return (0);
 }
 
-
-int get_next_line(char **line)
+int  get_next_line(char **line)
 {
     static char *content;
     char *buff;
@@ -48,7 +122,7 @@ int get_next_line(char **line)
         content = ft_strdup("");
     buff = malloc(sizeof(char) * 101);
     if (!(is_n(content)))
-        while ((r = read(0, buff, 100)))
+        while ((r = read(0, buff, 100)) > 0)
         {
             buff[r] = '\0';
             tmp = ft_strjoin(content, buff);
