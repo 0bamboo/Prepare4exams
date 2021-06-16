55811120473a50be4744727b931a6a3e3ce1b524
nowl01 <itsabdennacerama@gmail.com>
2021-06-16 22:06:01 +0100

    ex

--- Exam03/micro_paint/try.c
+++ Exam03/micro_paint/try.c
@@ -58,8 +58,8 @@ int ft_free(FILE *file, char *draw, char *msg)
 
 char *get_zone(FILE *file, t_zone *zone)
 {
-    int i;
     int ret;
+    int i;
     char *draw;
 
     if ((ret = fscanf(file, " %f %f %c\n", &zone->width, &zone->height, &zone->ch)) != 3)
@@ -70,22 +70,52 @@ char *get_zone(FILE *file, t_zone *zone)
         return NULL;
     if (zone->height <= 0 || zone->height > 300)
         return NULL;
-    if (!(draw = malloc(sizeof(char) * zone->height * zone->width)))
+    if (!(draw = malloc(sizeof(char) * zone->width * zone->height)))
         return NULL;
     i = 0;
-    while (i < zone->height * zone->width)
+    while (i < zone->width * zone->height)
         draw[i++] = zone->ch;
     return draw;
 }
 
+int drawing_shape(FILE *file, t_zone *zone, char *draw)
+{
+    int ret;
+    int ret1;
+    t_rect rect;
+    int i;
+    int j;
 
+    ret = 0;
+    while ((ret = fscanf(file, " %c %f %f %f %f %c\n", &rect.type, &rect.x, &rect.y, &rect.w, &rect.h, &rect.c)) == 6)
+    {
+        if (rect.h <= 0.00000000 || rect.w <= 0.00000000 || (rect.type != 'r' && rect.type != 'R'))
+            return 0;
+        j = 0;
+        while (j < zone->height)
+        {
+            i = 0;
+            while (i < zone->width)
+            {
+                ret1 = is_in_rect(i, j, &rect);
+                if ((rect.type == 'r' && ret1 == 2) || (rect.type == 'R' && ret1))
+                    draw[j * (int)zone->width + i] = rect.c;
+                i++;
+            }
+            j++;
+        }
+        return 1;
+    }
+    if (ret != 6)
+        return 0;
+    return 1;
+}
 
 int main(int ac, char **av)
 {
-    int i;
-    FILE *file;
-    t_zone zone;
     char *draw;
+    int i;
+    t_zone zone;
 
     if (ac != 2)
     {
