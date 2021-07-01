25385af925d072fadebc0094c7c89aabee1f3bfa
paayk <itsabdennacerama@gmail.com>
2021-07-01 21:28:32 +0100

    j

--- Exam03/mini_paint/try.c
+++ Exam03/mini_paint/try.c
@@ -1,22 +1,22 @@
 #include <math.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-typedef struct s_circle
+typedef struct s_cir
 {
     char type;
     float x;
     float y;
     float radius;
     char c;
-}t_circle;
+}t_cir;
 
-typedef struct  s_zone
+typedef struct s_zone
 {
     float width;
     float height;
-    char ch;
+    char c;
 }t_zone;
 
 void ft_putchar(char c)
@@ -24,53 +24,23 @@ void ft_putchar(char c)
     write(1, &c, 1);
 }
 
-void ft_putstr(char *str)
+void    ft_putstr(char *s)
 {
-    while (*str)
+    while (*s)
     {
-        ft_putchar(*str);
-        str++;
+        ft_putchar(*s);
+        s++;
     }
 }
 
-int ft_free(FILE *file, char *draw, char *msg)
-{
-    fclose(file);
-    if (draw)
-        free(draw);
-    if (msg)
-        ft_putstr(msg);
-    return 1;
-}
-
-char *get_zone(FILE *file, t_zone *zone)
-{
-    int ret;
-    int i;
-    char *draw;
-
-    if ((ret = fscanf(file, " %f %f %c\n", &zone->width, &zone->height, &zone->ch)) != 3)
-        return NULL;
-    if (ret == -1)
-        return NULL;
-    if (zone->width <= 0 || zone->height <= 0 || zone->width > 300 || zone->height > 300)
-        return NULL;
-    if (!(draw = malloc(sizeof(char) * zone->width * zone->height)))
-        return NULL;
-    i = 0;
-    while (i < zone->height *zone->width)
-        draw[i++] = zone->ch;
-    return draw; 
-}
-
-int is_in_circle(int x, int y, t_circle *circ)
+int is_in_cir(float x, float y, t_cir *c)
 {
     float distance;
 
-    distance = sqrtf(powf(x - circ->x, 2.) + powf(y - circ->y, 2.));
-    if (distance <= circ->radius)
+    distance = sqrtf(powf(x - c->x, 2.) + powf(y - c->y, 2.));
+    if (distance <= c->radius)
     {
-        if (circ->radius - distance < 1.)
+        if (c->radius - distance < 1.)
             return 2;
         return 1;
     }
@@ -79,16 +49,16 @@ int is_in_circle(int x, int y, t_circle *circ)
 
 int drawing_shape(FILE *file, t_zone *zone, char *draw)
 {
-    int ret1;
-    int ret;
     int i;
     int j;
-    t_circle circ;
+    int ret;
+    int ret1;
+    t_cir c;
 
     ret = 0;
-    while ((ret = fscanf(file, " %c %f %f %f %c\n", &circ.type, &circ.x, &circ.y, &circ.radius, &circ.c)) == 5)
+    while ((ret = fscanf(file, " %c %f %f %f %c\n", &c.type, &c.x, &c.y, &c.radius, &c.c)) == 5)
     {
-        if (circ.radius <= 0.00000000 || (circ.type != 'r' && circ.type != 'R'))
+        if (c.radius <= 0.00000000 || (c.type != 'c' && c.type != 'C'))
             return 0;
         j = 0;
         while (j < zone->height)
@@ -96,25 +66,53 @@ int drawing_shape(FILE *file, t_zone *zone, char *draw)
             i = 0;
             while (i < zone->width)
             {
-                ret1 = is_in_circle(i, j, &circ);
-                if ((ret1 == 2 && circ.type == 'c') || (circ.type == 'C' && ret1))
-                    draw[j * (int)zone->width + i] = circ.c;
+                ret1 = is_in_cir(i, j, &c);
+                if ((c.type == 'c' && ret1 == 2) || (c.type == 'C' && ret1))
+                    draw[j * (int)zone->width + i] = c.c;
                 i++;
             }
             j++;
         }
         return 1;
     }
-    if (ret != 5)
+    if (ret != -1)
         return 0;
+    return 1;
+}
+
+char *get_zone(FILE *file, t_zone *zone)
+{
+    int i;
+    int ret;
+    char *draw;
+
+    if ((ret = fscanf(file, " %f %f %c\n", &zone->width, &zone->height, &zone->c)) != 3)
+        return NULL;
+    if (zone->width <= 0 || zone->height <= 0 || zone->width > 300 || zone->height > 300)
+        return NULL;
+    if (!(draw = (char *)malloc(sizeof(char) * zone->width *zone->height)))
+        return NULL;
+    i = 0;
+    while (i < zone->width * zone->height)
+        draw[i++] = zone->c;
+    return draw;
+}
+
+int ft_free(FILE *file, char *draw, char *msg)
+{
+    fclose(file);
+    if (draw)
+        free(draw);
+    if (msg)
+        free(msg);
     return 1;
 }
 
 int main(int ac, char **av)
 {
-    int i;
-    char *draw;
     FILE *file;
+    char *draw;
+    int i;
     t_zone zone;
 
     if (ac != 2)
@@ -128,13 +126,13 @@ int main(int ac, char **av)
         return 1;
     }
     if (!(draw = get_zone(file, &zone)))
-        return(ft_free(file, NULL, "Error: Operation file corrupted\n"));
+        return (ft_free(file, NULL, "Error: Operation file corrupted\n"));
     if (!(drawing_shape(file, &zone, draw)))
         return (ft_free(file, NULL, "Error: Operation file corrupted\n"));
     i = 0;
     while (i < zone.height)
     {
-        write(1, draw + (i *(int)zone.width), zone.width);
+        write(1, draw + (i * (int)zone.width), zone.width);
         write(1, "\n", 1);
         i++;
     }
