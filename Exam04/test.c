81f1286f686ab1c86f95d6c76e3c6cdf670c140a
nowl01 <itsabdennacerama@gmail.com>
2021-12-15 14:19:27 +0100

    added subjects for exam 5

--- Exam04/test.c
+++ Exam04/test.c
@@ -1,31 +1,127 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/wait.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
-
-int g_i;
-int g_status = 0;
-char **g_tab;
+#include <stdlib.h>
 
 typedef enum s_type
 {
 	PIPE,
 	END
-} t_type;
+}t_type;
 
-t_type g_type;
+int g_i;
+int g_status = 0;
+char **g_tab;
+
 t_type g_prevtype;
+t_type g_type;
+
 int ft_strlen(char *str);
 int tab_len(char **tab);
-void ft_putstr(char *str, int fd, int line);
-void free_tab(char **tab);
 char *ft_strdup(char *str);
 char **push_tab(char **tab, char *str);
-void cd_cmd();
+void cd_cmd(void);
 int get_cmd(char **av);
 int exec_cmd(char **env, int fd, int pipefd[2]);
+void free_tab(char **tab);
+void ft_putstr(char *str, int fd, int line);
 
+
+int tab_len(char **tab)
+{
+	int i = -1;
+	while(tab[++i]);
+	return i;
+}
+
+int ft_strlen(char *str)
+{
+	int i = -1;
+	while (str[++i]);
+	return i;
+}
+
+void ft_putstr(char *str, int fd, int line)
+{
+	int i = -1;
+	while(str[++i])
+		write(fd, &str[i], 1);
+	if (line)
+		write(fd, "\n", 1);
+}
+
+void free_tab(char **tab)
+{
+	int i =-1;
+	while(tab[++i])
+		free(tab[i]);
+	free(tab);
+}
+
+char *ft_strdup(char *str)
+{
+	int i = -1;
+	char *new = malloc(sizeof(char) * (ft_strlen(str) + 1));
+	while(str[++i])
+		new[i] = str[i];
+	new[i] = 0;
+	return new;
+}
+
+
+char **push_tab(char **tab, char *str)
+{
+	int i = -1;
+	char **new = malloc(sizeof(char *) *(tab_len(tab) + 2));
+	while(tab[++i])
+		new[i] = ft_strdup(tab[i]);
+	new[i++] = ft_strdup(str);
+	free_tab(tab);
+	return new;
+}
+
+
+int get_cmd(char **av)
+{
+	g_type = END;
+	while (av[g_i])
+	{
+		if (!strcmp(av[g_i], "|"))
+		{
+			g_i++;
+			g_type = PIPE;
+			return 1;
+		}
+		else if (!strcmp(av[g_i], ";"))
+		{
+			g_i++;
+			g_type = END;
+			return 1;
+		}
+		g_tab = push_tab(g_tab, av[g_i]);
+		g_i++;
+	}
+	return 2;
+}
+
+void cd_cmd(void)
+{
+	if (tab_len(g_tab) == 2)
+	{
+		if (chdir(g_tab[0]) != 0)
+		{
+			ft_putstr("cant move to ", 2, 0);
+			ft_putstr(g_tab[0], 2, 1);
+			g_status = 1;
+		}
+	}
+	else
+	{
+		ft_putstr("wrong arg", 2, 1);
+		g_status = 1;
+	}
+}
 
 int exec_cmd(char **env, int fd, int pipefd[2])
 {
@@ -75,6 +171,7 @@ int exec_cmd(char **env, int fd, int pipefd[2])
 	return fd;
 }
 
+
 int main(int ac, char **av, char **env)
 {
 	int x, fd, pipefd[2];
@@ -90,7 +187,7 @@ int main(int ac, char **av, char **env)
 		{
 			if (g_tab[0] == NULL)
 			{
-				if ( x == 2)
+				if (x == 2)
 					break;
 			}
 			else
